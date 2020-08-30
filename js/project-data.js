53945ff8da074d9247aaafa042939c6f326c3c33
unknown <singhtalwinder790@gmail.com>
2020-08-30 14:05:30 +0530

    Added project cards

--- js/project-data.js
+++ js/project-data.js
@@ -2,56 +2,63 @@ const PROJECT_DATA = {
   project1: {
     name: `Project-1`,
     shortSummary: `This is project-1`,
-    detailedSummary: `Project provies solution to this problem. This can do so many
-        things`,
+    detailedSummary: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
     link: `https://www.google.com`,
   },
 
   project2: {
     name: `Project-2`,
     shortSummary: `This is project-2`,
-    detailedSummary: `Project provies solution to this problem. This can do so many
-        things`,
+    detailedSummary: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
     link: `https://www.google.com`,
   },
 
   project3: {
     name: `Project-3`,
-    shortSummary: `This is project-4`,
-    detailedSummary: `Project provies solution to this problem. This can do so many
-        things`,
+    shortSummary: `This is project-3`,
+    detailedSummary: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
     link: `https://www.google.com`,
   },
 
   project4: {
     name: `Project-4`,
     shortSummary: `This is project-4`,
-    detailedSummary: `Project provies solution to this problem. This can do so many
-        things`,
+    detailedSummary: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
     link: `https://www.google.com`,
   },
 
   project5: {
     name: `Project-5`,
     shortSummary: `This is project-5`,
-    detailedSummary: `Project provies solution to this problem. This can do so many
-        things`,
+    detailedSummary: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
     link: `https://www.google.com`,
   },
 
   project6: {
     name: `Project-6`,
     shortSummary: `This is project-6`,
-    detailedSummary: `Project provies solution to this problem. This can do so many
-        things`,
+    detailedSummary: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
     link: `https://www.google.com`,
   },
 
   project7: {
     name: `Project-7`,
     shortSummary: `This is project-7`,
-    detailedSummary: `Project provies solution to this problem. This can do so many
-        things`,
+    detailedSummary: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
     link: `https://www.google.com`,
   },
 };
